7f1e41c4cc43ae6f5c5128431e7084cc8ccfcba0
GiftedTerry <protechackers@gmail.com>
2024-06-27 12:44:45 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,33 +10,33 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
-global.ytname = "YT: Tech-God"
-global.socialm = "GitHub: techgod143"
-global.location = "India, Up, Rampur"
+global.ownernomer = "2347039853320"
+global.ownername = "King Gifted"
+global.ytname = "YT: GiftedHackers"
+global.socialm = "GitHub: GiftedTerry"
+global.location = "Africa, Lagos"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '2347039853320'  //creator number
+global.ownername = 'King Gifted' //owner name
+global.botname = 'Gifted wa Crasher ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'Tech-God\n\nContact: +917466008456'
+global.author = 'King-Gifted\n\nContact: +2347039853320'
 
 //console view/theme
-global.themeemoji = '🪀'
-global.wm = "Youtube Tech God."
+global.themeemoji = '😈'
+global.wm = "Youtube GiftedHackers."
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029Va9Ufzi8kyyEnEHvOm1h'
+global.link = 'https://whatsapp.com/channel/0029VaiVmtX4Y9lpUssihA1x'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
 
 //false=disable and true=enable
-global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
+global.autoRecording = true //auto recording
+global.autoTyping = true //auto typing
 global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = true //auto update bio
